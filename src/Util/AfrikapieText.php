ab7ddd92a66388ec1de628baa8f1e2403f362129
freepius <freepius@anarchos-semitas.net>
2015-02-27 17:00:19 +0100

    Full modification of AfrikapieText util (now, modif. instructions are in metadata files)

--- src/Util/AfrikapieText.php
+++ src/Util/AfrikapieText.php
@@ -2,106 +2,74 @@
 
 namespace App\Util;
 
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Yaml;
 
-/**
- * Utility allowing to transform a text in "Afrikapié" format.
- */
 class AfrikapieText
 {
-    const TAGS_CONV = [
-        'ar'    => '*<span dir="rtl">$1</span>*',
-        'arl'   => '*$1*',
-        'bird'  => '$1',
-        'ci'    => '$1',
-        'en'    => '*$1*',
-        'flora' => '$1',
-        'fp'    => '$1',
-        'frt'   => '$1',
-        'la'    => '*$1*',
-        'mc'    => '*$1*',
-        'na'    => '$1',
-        're'    => '$1',
-        'to'    => '$1',
-        'url'   => '[$1](http://$1)',
-        'wp'    => '<http://fr.wikipedia.org/wiki/$1>',
-    ];
+    /**
+     * The directory where to search the files
+     * (texts, "simple text" metadata and "enhanced text" metadata)
+     */
+    protected $dir;
 
-    protected $textDirectory;
-
-    public function __construct($textDirectory)
+    public function __construct($dir)
     {
-        $this->textDirectory = $textDirectory;
+        $this->dir = $dir;
     }
 
     public function findAndTransform($name)
     {
-        $fullName = "{$this->textDirectory}/$name.md";
+        $textPath     = "{$this->dir}/md/$name.md";
+        $simplePath   = "{$this->dir}/simple/$name.yml";
+        $enhancedPath = "{$this->dir}/enhanced/$name.yml";
 
-        // Check if the text exists
-        if (! file_exists($fullName) || ! is_readable($fullName))
-        {
-            throw new HttpException(404, "Le texte \"$name\" n'existe pas !");
-        }
+        $text             = self::readfile($textPath);
+        $simpleMetadata   = self::readfile($simplePath);
+        $enhancedMetadata = self::readfile($enhancedPath);
 
-        // Retrieve the text
-        $text = file_get_contents($fullName);
+        // Parse the 2 Yaml files
+        $parser           = new Yaml\Parser;
+        $simpleMetadata   = $parser->parse($simpleMetadata);
+        $enhancedMetadata = $parser->parse($enhancedMetadata);
 
-        // The "one shot" information
-        $oneShotInfo =
+        // Default values for some metadata
+        $defaultMetadata =
         [
-            // mandatory
             'intro' => null,
-            'title' => null,
-
-            // with a default value
             'image' => $name,
             'next'  => date('Y-m-d', strtotime("$name +1 day")),
             'prev'  => date('Y-m-d', strtotime("$name -1 day")),
         ];
 
-        // Retrieve and remove the "one shot" tags
-        foreach ($oneShotInfo as $tag => & $value) {
-            $tmp = self::getter($tag, $text);
-            if (null !== $tmp) { $value = $tmp; }
+        return $simpleMetadata + $defaultMetadata +
+        [
+            'simple'   => self::transformSimple($text, $simpleMetadata),
+            'enhanced' => self::transformEnhanced($text, $enhancedMetadata),
+        ];
+    }
+
+    /**
+     * Check if a file is readable (if not, throw a \RuntimeException).
+     * Then, open it and return its content.
+     *
+     * @param  string $path
+     * @return string
+     */
+    protected static function readfile($path)
+    {
+        if (!is_readable($path) || !is_file($path)) {
+            throw new \RuntimeException("Unable to open the file \"$path\"");
         }
-
-        // Transform the "multiple occurrences" tags
-        $tags = array_map([$this, 'tagify'], array_keys(self::TAGS_CONV));
-        $rpls = array_values(self::TAGS_CONV);
-        $text = preg_replace($tags, $rpls, $text);
-
-        return $oneShotInfo + ['content' => $text];
+        return file_get_contents($path);
     }
 
-    /**
-     * Transform 'mytag' into a regexp finding '<mytag>Value</mytag>'.
-     */
-    protected static function tagify($e)
+    protected static function transformSimple($text, array $metadata)
     {
-        return "|<$e>(.*)</$e>|U";
+        return $text;
     }
 
-    /**
-     * From a $text (passed by reference),
-     * find and return the value (or null) contained by a $tag.
-     * Then, remove this $tag from the $text.
-     *
-     * Example:
-     *
-     * $text = '<title>A title</title>...An other text...';
-     * $value = self::getter('title', $text);
-     *
-     * Then, $value === 'A title' and $text = '...An other text...'
-     */
-    protected static function getter($tag, & $text)
+    protected static function transformEnhanced($text, array $metadata)
     {
-        $text = preg_replace_callback(
-            self::tagify($tag),
-            function ($m) use (& $value) { $value = @ $m[1]; return; },
-            $text, 1
-        );
-
-        return $value;
+        return $text;
     }
 }
