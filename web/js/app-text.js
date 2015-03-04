70cc563bb6b7d3eadc12b80fed01c3cf71bb15a4
freepius <freepius@anarchos-semitas.net>
2015-03-04 23:38:12 +0100

    app-text.js: clean/improve the code

--- web/js/app-text.js
+++ web/js/app-text.js
@@ -1,32 +1,37 @@
-/*global $, L, asCarto, document, window, afrikapiePath, bingMapsAPIKey, currentPoint, textPoints */
+/*global $, L, document, window */
+/*global afrikapiePath, bingMapsAPIKey, currentPoint, textPoints */
 
 (function () {
-    "use strict";
+    'use strict';
 
-    var map = L.easyMap('map', {
+    /////////////////////
+    //     MAPPING     //
+    /////////////////////
+
+    var i,
+
+        // Map definition
+        map = L.easyMap('map', {
             baseLayers : {
                 enabled : ['Bing'],
                 first   : 'Landscape',
                 options : {bingMapsAPIKey : bingMapsAPIKey}
             },
             center   : currentPoint,
-            controls : {fullscreen: true, layers: true},
+            controls : {fullscreen: true, layers: false},
             zoom     : 10
         }),
-        i,
-        currentIcon = L.divIcon({
-            iconSize  : L.point(30, 30),
-            className : 'current-text-icon',
-            html      : '<i class="fa fa-circle"></i>'
-        }),
-        otherIcon   = L.divIcon({iconSize: L.point(20, 20), className: 'other-text-icon'});
 
-    function createOtherTextMarker(text) {
-        L.marker(textPoints[text], {icon: otherIcon})
-            .bindLabel('Aller au texte « ' + text + ' »')
-            .on('click', function () { window.location.assign('/' + text); })
-            .addTo(map);
-    }
+        // Icon definitions
+        currentIcon = L.divIcon({
+            className : 'current-text-icon',
+            html      : '<i class="fa fa-circle"></i>',
+            iconSize  : L.point(30, 30)
+        }),
+        otherIcon = L.divIcon({
+            className : 'other-text-icon',
+            iconSize  : L.point(20, 20)
+        });
 
     // Path of the Afrikapié travel
     L.polyline(afrikapiePath, {
@@ -36,25 +41,32 @@
         weight    : 8
     }).addTo(map);
 
-    // Marker for the current text point
+    // Marker for the current text
     L.marker(currentPoint, {clickable: false, icon: currentIcon}).addTo(map);
 
     // Markers for all other texts
-    for (i in textPoints) {
-        if (textPoints[i] !== currentPoint) { createOtherTextMarker(i); }
+    function createOtherTextMarker(slug) {
+        L.marker(textPoints[slug], {icon: otherIcon, riseOnHover: true})
+            .bindLabel('Aller au texte « ' + slug + ' »')
+            .on('click', function () { window.location.assign('/' + slug); })
+            .addTo(map);
     }
+
+    for (i in textPoints)
+        if (textPoints[i] !== currentPoint)
+            createOtherTextMarker(i);
+
+    ////////////////////////
+    // OTHER THAN MAPPING //
+    ////////////////////////
 
     /**
      * On mouseenter, hide the header content.
      * Useful to see the full background picture.
      */
-    $('body > header').hover(
-        function () { $(this).children('.inner').fadeOut(); },
-        function () { $(this).children('.inner').fadeIn(); }
-    );
-
-    $(document).ready(function () {
-
-    });
+    var header = document.querySelector('body > header'),
+        inner  = $(header.querySelector('.inner'));
+    header.onmouseenter = function () { inner.fadeOut(); };
+    header.onmouseleave = function () { inner.fadeIn(); };
 
 }());
