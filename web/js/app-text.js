63bc64c68081339f557b88be35b40d819c582773
freepius <freepius@anarchos-semitas.net>
2015-03-05 14:17:36 +0100

    Map: add a "max zoom" for the markers layer

--- web/js/app-text.js
+++ web/js/app-text.js
@@ -31,7 +31,10 @@
         otherIcon = L.divIcon({
             className : 'other-text-icon',
             iconSize  : L.point(20, 20)
-        });
+        }),
+
+        // Layer to group the markers
+        markers = L.layerGroup().addTo(map);
 
     // Path of the Afrikapié travel
     L.polyline(afrikapiePath, {
@@ -42,19 +45,32 @@
     }).addTo(map);
 
     // Marker for the current text
-    L.marker(currentPoint, {clickable: false, icon: currentIcon}).addTo(map);
+    L.marker(currentPoint, {clickable: false, icon: currentIcon, riseOnHover: true})
+        .addTo(markers);
 
     // Markers for all other texts
     function createOtherTextMarker(slug) {
         L.marker(textPoints[slug], {icon: otherIcon, riseOnHover: true})
             .bindLabel('Aller au texte « ' + slug + ' »')
             .on('click', function () { window.location.assign('/' + slug); })
-            .addTo(map);
+            .addTo(markers);
     }
 
     for (i in textPoints)
         if (textPoints[i] !== currentPoint)
             createOtherTextMarker(i);
+
+    // Under the zoom level 8, hide the markers
+    map.on('zoomend', function () {
+        if (map.getZoom() < 8) {
+            if (map.hasLayer(markers))
+                map.removeLayer(markers);
+            return;
+        }
+
+        if (!map.hasLayer(markers))
+            map.addLayer(markers);
+    });
 
     ////////////////////////
     // OTHER THAN MAPPING //
