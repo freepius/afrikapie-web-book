2c9d45652c46189cab77a01582afa8c821267f3b
freepius <freepius@anarchos-semitas.net>
2015-04-09 23:13:36 +0200

    Text page: move the "mapping part" javascript in a separated file

--- web/js/app-text.js
+++ web/js/app-text.js
@@ -1,75 +1,7 @@
-/*global $, L, buzz, document, window */
-/*global afrikapiePath, currentPoint, textPoints */
+/*global $, buzz, document */
 
 (function () {
     'use strict';
-
-    /////////////////////
-    //     MAPPING     //
-    /////////////////////
-
-    var i,
-
-        // Map definition
-        map = L.easyMap('map', {
-            baseLayers : {
-                enabled : ['BingAerial'],
-                first   : 'Landscape'
-            },
-            center   : currentPoint,
-            controls : {fullscreen: true},
-            zoom     : 10
-        }),
-
-        // Icon definitions
-        currentIcon = L.divIcon({
-            className : 'current-text-icon',
-            html      : '<i class="fa fa-circle"></i>',
-            iconSize  : L.point(30, 30)
-        }),
-        otherIcon = L.divIcon({
-            className : 'other-text-icon',
-            iconSize  : L.point(20, 20)
-        }),
-
-        // Layer to group the markers
-        markers = L.layerGroup().addTo(map);
-
-    // Path of the Afrikapié travel
-    L.polyline(afrikapiePath, {
-        clickable : false,
-        color     : 'brown',
-        opacity   : 0.7,
-        weight    : 8
-    }).addTo(map);
-
-    // Marker for the current text
-    L.marker(currentPoint, {clickable: false, icon: currentIcon, riseOnHover: true})
-        .addTo(markers);
-
-    // Markers for all other texts
-    function createOtherTextMarker(slug) {
-        L.marker(textPoints[slug], {icon: otherIcon, riseOnHover: true})
-            .bindLabel('Aller au texte « ' + slug + ' »')
-            .on('click', function () { window.location.assign('/' + slug); })
-            .addTo(markers);
-    }
-
-    for (i in textPoints)
-        if (textPoints[i] !== currentPoint)
-            createOtherTextMarker(i);
-
-    // Under the zoom level 8, hide the markers
-    map.on('zoomend', function () {
-        if (map.getZoom() < 7) {
-            if (map.hasLayer(markers))
-                map.removeLayer(markers);
-            return;
-        }
-
-        if (!map.hasLayer(markers))
-            map.addLayer(markers);
-    });
 
     ////////////////////
     //     SOUNDS     //
